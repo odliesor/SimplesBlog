742e73dde63e3bdc8d87cd9c02fca9ae43a9e5b8
odliesor <odliesor.infotec@gmail.com>
2020-10-22 11:34:37 -0300

    mensagem no commit

---
+++ php/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-login">
+        <form>
+            <h2>Login</h2>
+            <div class="grupo-form">
+                <label for="email">E-mail: </label>
+            <input class="imput-texto" type="text" name="email" id="email">
+            </div>
+        <div class="grupo-form">
+            <label for="senha">Senha: </label>
+            <input type="password" name="senha" id="senha">
+        </div>
+        type="submit" value
+        </form>
+    
+    </div>
+</body>
+</html>
